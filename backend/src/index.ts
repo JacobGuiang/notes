70727a0bc2188a412783a23e8778e49f64f10588
JacobGuiang <jacob.guiang1@gmail.com>
2024-03-18 16:38:21 -0500

    Improve error handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,13 @@ import app from './app';
 import config from './config/config';
 import logger from './config/logger';
 
-app.listen(config.port, () => {
+const server = app.listen(config.port, () => {
   logger.info(`Listening to port ${config.port}`);
+});
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM signal received: closing HTTP server');
+  server.close(() => {
+    logger.info('HTTP server closed');
+  });
 });
