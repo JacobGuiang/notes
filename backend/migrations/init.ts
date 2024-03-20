43c3affd83008ea98ea644a68795ccf3fc6e0c71
JacobGuiang <jacob.guiang1@gmail.com>
2024-03-20 13:55:16 -0500

    Add Knex to backend for SQL migrations

---
+++ backend/migrations/init.ts
@@ -0,0 +1,13 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('users', function (table) {
+    table.increments('id');
+    table.string('username').unique().notNullable();
+    table.text('password').notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('users');
+}
