6f5d44b9c6c775aaee17ff3f1959f52c94853613
JacobGuiang <jacob.guiang1@gmail.com>
2024-03-21 12:56:19 -0500

    Add kysely query builder and update createUser service

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,31 +1,26 @@
 import { StatusCodes } from 'http-status-codes';
-import postgres from '@/config/postgres';
 import ApiError from '@/utils/ApiError';
-import format from 'pg-format';
+import { NewUser } from '@/types/db';
+import { db } from '@/config/db';
 
-interface userBody {
-  username: string;
-  password: string;
-}
+const createUser = async (user: NewUser) => {
+  user.username = user.username.toLowerCase();
 
-const createUser = async (userBody: userBody) => {
-  userBody.username = userBody.username.toLowerCase();
-
-  let res = await postgres.query(
-    format('SELECT * FROM users WHERE username = %L', userBody.username)
-  );
-
-  if (res.rows.length > 0) {
+  if (
+    await db
+      .selectFrom('user')
+      .selectAll()
+      .where('username', '=', user.username)
+      .executeTakeFirst()
+  ) {
     throw new ApiError(StatusCodes.BAD_REQUEST, 'Username already taken');
   }
 
-  res = await postgres.query(
-    format(
-      'INSERT (%L) INTO users VALUES (%L)',
-      Object.keys(userBody),
-      Object.values(userBody)
-    )
-  );
-
-  return res.rows[0];
+  return db
+    .insertInto('user')
+    .values(user)
+    .returningAll()
+    .executeTakeFirstOrThrow();
 };
+
+export default { createUser };
