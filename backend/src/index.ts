51bed273b4a93fcd149d527b7813214227f2e248
JacobGuiang <jacob.guiang1@gmail.com>
2024-03-20 21:37:20 -0500

    Improve graceful shutdown in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,10 +7,39 @@ const server = app.listen(config.port, () => {
   logger.info(`Listening to port ${config.port}`);
 });
 
+const exitHandler = () => {
+  if (postgres) {
+    postgres.end();
+    logger.info('Postgres pool closed');
+  }
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error: Error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGINT', exitHandler);
+
 process.on('SIGTERM', () => {
-  logger.info('SIGTERM signal received: closing HTTP server');
-  postgres.end();
-  server.close(() => {
-    logger.info('HTTP server closed');
-  });
+  logger.info('SIGTERM received');
+  if (postgres) {
+    postgres.end();
+    logger.info('Postgres pool closed');
+  }
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed');
+    });
+  }
 });
