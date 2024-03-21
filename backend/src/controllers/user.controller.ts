6b5273256508770b886c107388fbce5a13068fd0
JacobGuiang <jacob.guiang1@gmail.com>
2024-03-21 01:50:12 -0500

    WIP add controller and service for user feature

---
+++ backend/src/controllers/user.controller.ts
@@ -0,0 +1,23 @@
+import catchAsync from '@/utils/catchAsync';
+
+const createUser = catchAsync(async (req: Request, res: Response) => {
+  // TODO
+});
+
+const getUsers = catchAsync(async (req: Request, res: Response) => {
+  // TODO
+});
+
+const getUser = catchAsync(async (req: Request, res: Response) => {
+  // TODO
+});
+
+const updateUser = catchAsync(async (req: Request, res: Response) => {
+  // TODO
+});
+
+const deleteUser = catchAsync(async (req: Request, res: Response) => {
+  // TODO
+});
+
+export default { createUser, getUsers, getUser, updateUser, deleteUser };
