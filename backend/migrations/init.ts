5ab2cc62ec305c9e6e12cddec5e013dd81b4093a
JacobGuiang <jacob.guiang1@gmail.com>
2024-03-28 13:21:58 -0500

    Add note to database schema and types

--- backend/migrations/init.ts
+++ backend/migrations/init.ts
@@ -1,13 +1,28 @@
 import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable('user', function (table) {
-    table.increments('id');
-    table.string('username').unique().notNullable();
-    table.text('password').notNullable();
-  });
+  return knex.schema
+    .createTable('user', function (table) {
+      table.increments('id');
+      table.string('username').unique().notNullable();
+      table.text('password').notNullable();
+    })
+    .createTable('note', function (table) {
+      table.increments('id');
+      table.integer('user_id').unsigned().notNullable();
+      table
+        .foreign('user_id')
+        .references('id')
+        .inTable('user')
+        .onDelete('cascade');
+      table.text('content').notNullable();
+      table
+        .timestamp('updated_at', { precision: 0 })
+        .defaultTo(knex.fn.now())
+        .notNullable();
+    });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable('user');
+  return knex.schema.dropTable('note').dropTable('user');
 }
--- backend/src/types/db.d.ts
+++ backend/src/types/db.d.ts
@@ -1,7 +1,14 @@
-import { Generated, Insertable, Selectable, Updateable } from 'kysely';
+import {
+  ColumnType,
+  Generated,
+  Insertable,
+  Selectable,
+  Updateable,
+} from 'kysely';
 
 export interface Database {
   user: UserTable;
+  note: NoteTable;
 }
 
 export interface UserTable {
@@ -13,3 +20,18 @@ export interface UserTable {
 export type User = Selectable<UserTable>;
 export type NewUser = Insertable<UserTable>;
 export type UserUpdate = Updateable<UserTable>;
+
+export interface NoteTable {
+  id: Generated<number>;
+  user_id: number;
+  content: string;
+
+  // selected as a Date
+  // can never be inserted
+  // updated as RawBuilder<unknown> because im using sql`now()`
+  updated_at: ColumnType<Date, never, RawBuilder<unknown>>;
+}
+
+export type Note = Selectable<NoteTable>;
+export type NewNote = Insertable<NoteTable>;
+export type NoteUpdate = Updateable<NoteTable>;
