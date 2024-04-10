9ce110d4d819d01d1719d3f6290fbc71cb385bc3
JacobGuiang <jacob.guiang1@gmail.com>
2024-04-10 15:22:43 -0500

    Update Tailwind classes in AuthLayout.tsx and input.tsx
    Change h-[100vh] to h-screen
    Increase border size of input

--- frontend/src/components/Layout/AuthLayout.tsx
+++ frontend/src/components/Layout/AuthLayout.tsx
@@ -6,7 +6,7 @@ interface AuthLayoutProps {
 
 export const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <div className="h-[100vh] flex flex-col items-center justify-center space-y-8">
+    <div className="h-screen flex flex-col items-center justify-center space-y-8">
       <h1 className="text-primary text-8xl">Notes</h1>
       {children}
     </div>
